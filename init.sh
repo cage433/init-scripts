fd49f777b673fb16c2030b484660d477b834ee09
Alex McGuire <cage433@gmail.com>
2010-06-14 23:36:08 +0100

    Add ctags to init script

--- init.sh
+++ init.sh
@@ -1,8 +1,12 @@
 if [ ! -e $HOME/.vim ]; then
 	echo "Creating link to .vim "
-	ln -s $HOME/github/alex-init-scripts/dot-vim $HOME/.vim
+	ln -s $HOME/github/alex-init-scripts/vim/dot-vim $HOME/.vim
 fi
 if [ ! -e $HOME/.vimrc ]; then
 	echo "Creating link to .vimrc "
-	ln -s $HOME/github/alex-init-scripts/dot-vimrc $HOME/.vimrc
+	ln -s $HOME/github/alex-init-scripts/vim/dot-vimrc $HOME/.vimrc
+fi
+if [ ! -e $HOME/.ctags ]; then
+	echo "Creating link to .ctags "
+	ln -s $HOME/github/alex-init-scripts/dot-ctags $HOME/.ctags
 fi
