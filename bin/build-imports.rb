7f01241d87b46c02edd54f2475db75cdab540569
Alex McGuire <cage433@gmail.com>
2011-12-16 22:38:00 +0000

    minor fixes

--- bin/build-imports.rb
+++ bin/build-imports.rb
@@ -17,15 +17,18 @@ def scala_root
   end
   scala_root || raise("Can't find scala home")
 end 
-def scala_library_jar 
-  "#{scala_root}/lib/scala-library.jar"
+def scala_library_jars
+  ["#{scala_root}/lib/scala-library.jar", "#{scala_root}/lib/scala-compiler.jar"] 
 end
 
 def package_name(file)
   dirs = File.dirname(file).split("/").reverse
   i = dirs.index("src") || dirs.index("tests")|| dirs.index("model-src")
-  raise dirs.join("/") unless i
-  dirs[0..i-1].reverse.join(".")
+  if i then
+    dirs[0..i-1].reverse.join(".")
+  else
+    nil
+  end
 end
 
 def is_jar_to_extract(file)
@@ -33,12 +36,12 @@ def is_jar_to_extract(file)
 end
 
 Find.find(Dir.pwd) do |file|
-  if FileTest.directory?(file) && file =~ /\/project/ then
-    Find.prune
-  elsif file =~ /.*\.scala$/ then
+  if file =~ /.*\.scala$/ then
     IO.readlines(file).each do |line|
       if line =~ /\s*(class|trait|object)\s*(\w+)\s*(private)?\s*(((extends)|(with))\s+\w+\s*)*(\{|\[|\(|$)/ then
-        project_classes.unshift([$2, package_name(file)])
+        if package_name(file) then 
+          project_classes.unshift([$2, package_name(file)])
+        end
       end
     end
   elsif do_external_jars && is_jar_to_extract(file) then
@@ -57,6 +60,7 @@ end
 def extract_jars_to_file(jar_files, output_file)
   File.open(output_file, "w") do |f|
     jar_files.each do |file|
+      puts file
 
       `jar tvf #{file}`.split("\n").each do |line|
         if line =~ /(\S+)\.class/
@@ -76,6 +80,6 @@ end
 if do_external_jars then
   java_classes_jar = ["/System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK/Classes/classes.jar", "/usr/local/jdk/jre/lib/rt.jar"].find{|f| File.exists?(f)} || raise("Can't find java classes")
   extract_jars_to_file([java_classes_jar], $java_packages)
-  extract_jars_to_file([scala_library_jar], $scala_packages)
+  extract_jars_to_file(scala_library_jars, $scala_packages)
   extract_jars_to_file($project_jars,  $external_packages)
 end
