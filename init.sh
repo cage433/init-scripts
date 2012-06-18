91b4fae857ed75050e419611689e961cdda675d0
Alex McGuire <alex.mcguire@trafigura.com>
2012-06-18 11:47:54 +0100

    Add .ackrc

--- init.sh
+++ init.sh
@@ -14,6 +14,10 @@ if [ ! -e $HOME/.tmux.conf ]; then
 	echo "Creating link to .tmux.conf"
 	ln -s $PWD/dot-tmux-dot-conf $HOME/.tmux.conf
 fi
+if [ ! -e $HOME/.ackrc ]; then
+	echo "Creating link to .ackrc"
+	ln -s $PWD/dot-ackrc $HOME/.ackrc
+fi
 if [ ! -e $HOME/.bashrc.alex ]; then
 	echo "Creating link to .bashrc.alex - Remember to source from .bashrc"
 	ln -s $PWD/dot-bashrc-dot-alex $HOME/.bashrc.alex
