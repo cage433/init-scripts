5ce51a68529b7bb60d785e69feee3e6b499775b5
Alex.McGuire <alex.mcguire@trafigura.com>
2014-10-27 11:24:26 +0000

    Remove unused script

--- bin/starling-classpath.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/ruby -w
-
-jars = `find . -name '*.jar'`.split("\n")
-class_dirs = `find . -name classes -type d`.split("\n")
-test_class_dirs = `find . -name test-classes -type d`.split("\n")
-classpath = (class_dirs + test_class_dirs + jars).join(":")
-puts classpath
-
