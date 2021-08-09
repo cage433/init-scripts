d8ea65cac3412b4bc55d0600c5a4304d7f676565
cage433 <cage433@gmail.com>
2021-08-09 08:45:25 +0100

    tm.sh

---
+++ bin/tm.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+tmuxinator s topaz
+tmuxinator s topaz2
+tmuxinator s topaz3
+tmuxinator s topaz4
+tmuxinator s topaz5
+tmuxinator s pull-requests
+tmuxinator s general
+tmux attach -d -t topaz
+
