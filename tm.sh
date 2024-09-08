b374dc2a8d4e687d2000614ae35e31235c59b17b
cage433 <cage433@gmail.com>
2024-09-08 22:07:00 +0100

    various - including .emacs

---
+++ tm.sh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+
+tmuxinator s topaz
+tmuxinator s topaz2
+tmuxinator s topaz3
+tmuxinator s topaz4
+tmuxinator s topaz5
+tmuxinator s topaz6
+tmuxinator s topaz7
+tmuxinator s topaz8
+tmuxinator s topaz9
+tmuxinator s topaz10
+tmuxinator s pull-requests
+tmuxinator s pull-requests2
+tmuxinator s general
+tmuxinator s vortex
+tmux attach -d -t topaz
+
